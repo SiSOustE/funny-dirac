2eb650633c5bcf0a629b2ff6514b342ec2f7d9fc
SiSOustE <150558984+SiSOustE@users.noreply.github.com>
2025-07-09 01:13:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
+import Character from "./Character/Character";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <Character />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
