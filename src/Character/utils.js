fa45440dfee342e4b80e041f2e3812a7bc566a82
SiSOustE <150558984+SiSOustE@users.noreply.github.com>
2025-07-08 16:23:31 +0300

    Initial commit

---
+++ src/Character/utils.js
@@ -0,0 +1,7 @@
+// https://stackoverflow.com/questions/5915096/get-a-random-aitem-from-array
+export const getRandomElementOfArray = (array) => {};
+array[Math.floor(Math.random() * array.length)];
+
+// https://css-tricks.com/snippets/javascript/random-hex-color/
+export const getRandomHexColor = () =>
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
