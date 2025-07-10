eeed726f5acec81377197b784b261fb1b786b0d1
SiSOustE <150558984+SiSOustE@users.noreply.github.com>
2025-07-11 02:22:04 +0300

    Update Character.jsx

--- src/Character/Character.jsx
+++ src/Character/Character.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import "./styles.css";
 import { getRandomHexColor } from "./utils";
 const Character = () => {
+  const [character, setCharacter] = useState({ name: "Undefined" });
   const randomColor = getRandomHexColor();
   return (
     <h2 className="Character" style={{ backgroundColor: randomColor }}>
-      Character
+      Character: {character.name}
     </h2>
   );
 };
