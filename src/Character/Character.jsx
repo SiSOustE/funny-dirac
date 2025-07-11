c735eb5d98b0a0da91956d24408f0593142223d9
SiSOustE <150558984+SiSOustE@users.noreply.github.com>
2025-07-11 14:49:31 +0300

    Update Character.jsx

--- src/Character/Character.jsx
+++ src/Character/Character.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
 import "./styles.css";
 import { getRandomHexColor } from "./utils";
+import { API_URL } from "./consts";
 const Character = () => {
   const [character, setCharacter] = useState({ name: "Undefined" });
   const randomColor = getRandomHexColor();
+  const getRandomCharacterHandler = () => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        // data processing
+      });
+
+    console.log("getRandomCharacterHandler");
+  };
   return (
-    <h2 className="Character" style={{ backgroundColor: randomColor }}>
-      Character: {character.name}
-    </h2>
+    <div>
+      <h2 className="Character" style={{ backgroundColor: randomColor }}>
+        Character: {character.name}
+      </h2>
+      <button onclick={getRandomCharacterHandler}>Get Random Character</button>
+    </div>
   );
 };
 
