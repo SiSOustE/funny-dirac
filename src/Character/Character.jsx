43305b71b4d07dbdef65f6310d4f50affba476d2
SiSOustE <150558984+SiSOustE@users.noreply.github.com>
2025-07-14 15:44:08 +0300

    Update Character.jsx → hide

--- src/Character/Character.jsx
+++ src/Character/Character.jsx
@@ -1,22 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./styles.css";
 import { getRandomHexColor, getRandomElementOfArray } from "./utils";
 import { API_URL } from "./consts";
 const Character = () => {
+  const intervalRef = useRef(undefined);
   const [character, setCharacter] = useState({ name: "Undefined" });
+  const [visibility, setVisibility] = useState(true);
   const randomColor = getRandomHexColor();
 
   useEffect(() => {
     console.log("Component: Mount");
     getRandomCharacter();
 
-    setInterval(() => {
+    intervalRef.current = setInterval(() => {
       console.log("setInterval");
       getRandomCharacter();
     }, 3000);
 
     return () => {
       console.log("Component: Unmount");
+      clearInterval(intervalRef.current);
     };
   }, []);
 
@@ -37,8 +40,18 @@ const Character = () => {
     getRandomCharacter();
     console.log("getRandomCharacterHandler");
   };
+
+  const hideComponentHandler = () => {
+    setVisibility(false);
+  };
+
+  if (!visibility) {
+    return null;
+  }
+
   return (
     <>
+      <button onClick={hideComponentHandler}>Hide Component</button>
       <h2 className="Character" style={{ backgroundColor: randomColor }}>
         Character: {character.name}
       </h2>
